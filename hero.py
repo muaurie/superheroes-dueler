be6ceb95d1149b17c44cd2dcfd4a687e145bf245
Cherish Spikes <puppuponline@gmail.com>
2023-09-27 13:53:29 -0700

    Add files via upload

--- hero.py
+++ hero.py
@@ -1,14 +1,22 @@
 import random
 from Opponent import Opponent
+from ability import Ability
+from armor import Armor
 
 class Hero:
     def __init__(self, name, starting_health=100):
        ''' Instance properties:
+       abilities: List
+       armors: List
         name: String
         starting_health: Integer
         current_health: Integer
         '''
+       #abilities and armors are lists, don't have starting values and are set to empty upon initialization
        #assign name of hero, known required value
+       self.abilities = list()
+       self.armors = list()
+       
        self.name = name
        #pass in starting health
        self.starting_health = starting_health
@@ -19,12 +27,66 @@ class Hero:
         outcome = ['wins','loses','draw']
         result = random.choice(outcome)
         print(f"{self.name} {result} against {opponent.name}")
-    #call constructor
-if __name__ == "__main__": #this code only runs if directly called
-    #here the if[] prevents this from being run when imported by another script
-    #later when import the hero class, don't test with this!!
+    def add_ability(self, ability):
+        self.abilities.append(ability)
+    def attack(self):
+        #defin new method to USE hero's ability with attack
+        #attack value defined in ability.py
+        total_damage = 0
+        #loop thru ea. ability
+        for ability in self.abilities:
+        #add damage of ea. ability to running total
+            total_damage += Armor.attack()
+        return total_damage
+    def add_armor(self, armor):
+        #recieve armor object and append to self.armors
+        self.armors.append(armor)
+    def defend(self, damage):
+        self.damage = damage
+        #add and init damage
+        #state base armor in case none
+        total_armor = 0
+        #loop through ea. armor
+        for armor in self.armors:
+            #add block value of each armor to the total
+            total_armor += armor.block()
+        return total_armor
+    def take_damage(self, damage):
+        #call self.defend to calc damage reduction
+        damage_reduction = self.defend(damage)
+        #calc actual damage
+        if damage < 0:
+            #if damage is negative, convert integer
+           positive_damage = abs(damage)
+           return positive_damage
+        else:
+            #print ("You didn't do enough damage!")
+             actual_damage = damage - damage_reduction
+        #subtract total from self.current_health
+        self.current_health -= actual_damage
+        return actual_damage
+    def is_alive(self):
+            return self.current_health > 0  
+
+
+
+    #call constructor test
+if __name__ == "__main__": 
+    ability = Ability("Great Debug Bug", 50)
+    #ability1 = Ability("Monkey throw", 60)
     hero = Hero("Grace Hopper", 200)
+    hero.add_ability(ability)
+    #hero.add_ability(ability1)
+    shield = Armor("Copper Shield", 75)
+    hero.add_armor(shield)
+    hero.take_damage(10)
+    print(hero.is_alive())
+    hero.take_damage(150000)
+    print(hero.is_alive())
+
     opponent = Opponent("Monkey Man", 150)
     hero.fight(opponent)
+    ###### simulate attack
+
     
     
